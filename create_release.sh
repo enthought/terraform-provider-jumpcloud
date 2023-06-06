385b9391703c5b8603c8d2582a389b00e98a8b9d
Didrik Pinte <dpinte@enthought.com>
2023-06-06 11:37:47 -0500

    DEV: update target platforms

--- create_release.sh
+++ create_release.sh
@@ -11,17 +11,32 @@ echo "Creating source archive"
 tar -cvf "${RELEASE_DIR}/source-$1.tar.gz" ./
 
 mkdir -p "${RELEASE_DIR}"
-for GOARCH in 386 amd64
+for goarch in amd64 arm64
 do
-  for GOOS in darwin linux
+  for goos in darwin
   do
-    echo "Creating release binary for ${GOOS} / ${GOARCH}"
-  	env GOOS=${GOOS} GOARCH=${GOARCH} go build -o terraform-provider-jumpcloud
-    TARGET_FILENAME="${RELEASE_DIR}/terraform-provider-jumpcloud_${1}_${GOOS}_${GOARCH}.tar.gz"
-    tar -cvf "${TARGET_FILENAME}" terraform-provider-jumpcloud
-    TARGETS="$TARGETS $TARGET_FILENAME"
+    echo "creating release binary for ${goos} / ${goarch}"
+  	env goos=${goos} goarch=${goarch} go build -o terraform-provider-jumpcloud
+    target_filename="${release_dir}/terraform-provider-jumpcloud_${1}_${goos}_${goarch}.tar.gz"
+    tar -cvf "${target_filename}" terraform-provider-jumpcloud
+    targets="$targets $target_filename"
   done
 done
+
+for goarch in amd64
+do
+  for goos in linux
+  do
+    echo "creating release binary for ${goos} / ${goarch}"
+  	env goos=${goos} goarch=${goarch} go build -o terraform-provider-jumpcloud
+    target_filename="${release_dir}/terraform-provider-jumpcloud_${1}_${goos}_${goarch}.tar.gz"
+    tar -cvf "${target_filename}" terraform-provider-jumpcloud
+    targets="$targets $target_filename"
+  done
+done
+
+
+
 
 echo "Creating md5 checksum"
 md5sum ${TARGETS} > "${RELEASE_DIR}/checksum-md5.txt"
