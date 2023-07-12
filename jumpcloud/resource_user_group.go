7208eb709f9514892eb3c6cdb57877731bbe4774
Martin Lindner <mlindner@enthought.com>
2023-07-12 16:33:34 +0900

    Remove user_group attributes support

--- jumpcloud/resource_user_group.go
+++ jumpcloud/resource_user_group.go
@@ -2,10 +2,7 @@ package jumpcloud
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"net/http"
 
 	jcapiv2 "github.com/TheJumpCloud/jcapi-go/v2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -22,32 +19,6 @@ func resourceUserGroup() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"attributes": {
-				Type:     schema.TypeMap,
-				Optional: true,
-				Computed: true,
-				ForceNew: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"posix_groups": {
-							Type: schema.TypeString,
-							// PosixGroups cannot be edited after group creation.
-							ForceNew: true,
-							Optional: true,
-						},
-						// enable_samba has a more complicated lifecycle,
-						// Commenting out for now as it is ignored in CRU by the JCAPI
-						// From Jumpcloud UI:
-						// Samba Authentication must be configured in the
-						// JumpCloud LDAP Directory and LDAP sync must be enabled
-						// on this group before Samba Authentication can be enabled.
-						// "enable_samba": {
-						// 	Type:     schema.TypeBool,
-						// 	Optional: true,
-						// },
-					},
-				},
-			},
 		},
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
@@ -60,12 +31,6 @@ func resourceUserGroupCreate(d *schema.ResourceData, m interface{}) error {
 	client := jcapiv2.NewAPIClient(config)
 
 	body := jcapiv2.UserGroupPost{Name: d.Get("name").(string)}
-
-	// For Attributes.PosixGroups, only the first member of the slice
-	// is considered by the JCAPI
-	if attr, ok := expandAttributes(d.Get("attributes")); ok {
-		body.Attributes = attr
-	}
 
 	req := map[string]interface{}{
 		"body": body,
@@ -82,64 +47,27 @@ func resourceUserGroupCreate(d *schema.ResourceData, m interface{}) error {
 	return resourceUserGroupRead(d, m)
 }
 
-// resourceUserGroupRead uses a helper function that consumes the
-// JC's HTTP API directly; the groups' attributes need to be kept in state
-// as they are required for resourceUserGroupUpdate and the current
-// implementation of the JC SDK doesn't support their retrieval
 func resourceUserGroupRead(d *schema.ResourceData, m interface{}) error {
 	config := m.(*jcapiv2.Configuration)
+	client := jcapiv2.NewAPIClient(config)
 
-	group, ok, err := userGroupReadHelper(config, d.Id())
+	res, _, err := client.UserGroupsApi.GroupsUserGet(context.TODO(), d.Id(), "", headerAccept, nil)
+
 	if err != nil {
+		if err.Error() == "EOF" {
+			d.SetId("")
+			return nil
+		}
 		return err
 	}
 
-	if !ok {
-		// not found
-		d.SetId("")
-		return nil
-	}
+	d.SetId(res.Id)
 
-	d.SetId(group.ID)
-	if err := d.Set("name", group.Name); err != nil {
-		return err
-	}
-	if err := d.Set("attributes", flattenAttributes(&group.Attributes)); err != nil {
+	if err := d.Set("name", res.Name); err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func userGroupReadHelper(config *jcapiv2.Configuration, id string) (ug *UserGroup,
-	ok bool, err error) {
-
-	req, err := http.NewRequest(http.MethodGet,
-		config.BasePath+"/usergroups/"+id, nil)
-	if err != nil {
-		return
-	}
-
-	req.Header.Add("x-api-key", config.DefaultHeader["x-api-key"])
-	if config.DefaultHeader["x-org-id"] != "" {
-		req.Header.Add("x-org-id", config.DefaultHeader["x-org-id"])
-	}
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Accept", "application/json")
-
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode == http.StatusNotFound {
-		return
-	}
-
-	ok = true
-	err = json.NewDecoder(res.Body).Decode(&ug)
-	return
 }
 
 func resourceUserGroupUpdate(d *schema.ResourceData, m interface{}) error {
@@ -147,17 +75,11 @@ func resourceUserGroupUpdate(d *schema.ResourceData, m interface{}) error {
 	client := jcapiv2.NewAPIClient(config)
 
 	body := jcapiv2.UserGroupPost{Name: d.Get("name").(string)}
-	if attr, ok := expandAttributes(d.Get("attributes")); ok {
-		body.Attributes = attr
-	} else {
-		return errors.New("unable to update, attributes not expandable")
-	}
 
 	req := map[string]interface{}{
 		"body": body,
 	}
-	// behaves like PUT, will fail if
-	// attributes.posixGroups isn't sent, see GODOC
+
 	_, res, err := client.UserGroupsApi.GroupsUserPatch(context.TODO(),
 		d.Id(), "", headerAccept, req)
 	if err != nil {
